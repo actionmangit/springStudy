79acf7b76f1b6a4f8009ada04bb38f99f1d05f5e
kuhelhel <kuhelhel@www.master.com>
2017-04-08 17:00:23 +0900

    곱 계산 컬백함수 추가

--- study/src/main/java/com/hyoungki/learningtest/template/Calculator.java
+++ study/src/main/java/com/hyoungki/learningtest/template/Calculator.java
@@ -24,24 +24,23 @@ public class Calculator {
 		
 	}
 	
-//	public Integer calcSum(String filepath) throws IOException {
-//		BufferedReaderCallback	sumCallback	= new BufferedReaderCallback() {
-//
-//			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-//				Integer		sum		= 0;
-//				String		line	= null;
-//				
-//				while((line = br.readLine()) != null) {
-//					sum	+= Integer.valueOf(line);
-//				}
-//				return sum;
-//			}
-//			
-//		};
-//		
-//		return fileReadTemplate(filepath, sumCallback);
-//		
-//	}
+	public Integer calcMultiply(String filepath) throws IOException {
+		BufferedReaderCallback	multiplyCallback	= new BufferedReaderCallback() {
+
+			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+				Integer		multiply	= 1;
+				String		line		= null;
+				
+				while((line = br.readLine()) != null) {
+					multiply	*= Integer.valueOf(line);
+				}
+				return multiply;
+			}
+			
+		};
+		
+		return fileReadTemplate(filepath, multiplyCallback);
+	}
 	
 	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
 		BufferedReader	br		= null;
