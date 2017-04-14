e91632a907c6d20f8ecff9627b854e92d235b2ff
kuhelhel <kuhelhel@vc.fncp.or.kr>
2017-04-15 07:01:14 +0900

    1. 마지막 커밋

--- study/src/main/java/com/hyoungki/learningtest/template/Calculator.java
+++ study/src/main/java/com/hyoungki/learningtest/template/Calculator.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Calculator {
 	
 	public Integer calcSum(String filepath) throws IOException {
-		LineCallback	sumCallback	= new LineCallback() {
+		LineCallback<Integer>	sumCallback	= new LineCallback<Integer>() {
 
 			public Integer doSomethingWithLine(String line, Integer value) {
 				return value + Integer.valueOf(line);
@@ -16,11 +16,10 @@ public class Calculator {
 		};
 		
 		return lineReadTemplate(filepath, sumCallback, 0);
-		
 	}
 	
 	public Integer calcMultiply(String filepath) throws IOException {
-		LineCallback	multiplyCallback	= new LineCallback() {
+		LineCallback<Integer>	multiplyCallback	= new LineCallback<Integer>() {
 
 			public Integer doSomethingWithLine(String line, Integer value) {
 				return value * Integer.valueOf(line);
@@ -31,12 +30,24 @@ public class Calculator {
 		return lineReadTemplate(filepath, multiplyCallback, 1);
 	}
 	
-	public Integer lineReadTemplate (String filepath, LineCallback callback, int initVal) throws IOException {
+	public String concatenate(String filepath) throws IOException {
+		LineCallback<String>	concatenateCallback	= new LineCallback<String>() {
+
+			public String doSomethingWithLine(String line, String value) {
+				return value + line;
+			}
+			
+		};
+		
+		return lineReadTemplate(filepath, concatenateCallback, "");
+	}
+	
+	public <T> T lineReadTemplate (String filepath, LineCallback<T> callback, T initVal) throws IOException {
 		BufferedReader	br		= null;
 		
 		try {
 			br	= new BufferedReader(new FileReader(filepath));
-			Integer		res		= initVal;
+			T			res		= initVal;
 			String		line	= null;
 			while((line = br.readLine()) != null) {
 				res	= callback.doSomethingWithLine(line, res);
