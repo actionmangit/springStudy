6178854f81877e421aa19de00ef967abbf1146d8
kuhelhel <kuhelhel@vc.fncp.or.kr>
2017-04-18 08:03:41 +0900

    1. jdbcTemplate 사용

--- study/src/main/java/com/hyoungki/study/dao/UserDao.java
+++ study/src/main/java/com/hyoungki/study/dao/UserDao.java
@@ -8,16 +8,23 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 import com.hyoungki.study.domain.User;
 
 public class UserDao {	
 
 	private JdbcContext jdbcContext;
+	private JdbcTemplate jdbcTemplate;
+	
 	private DataSource	dataSource;
 	
 	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate	= new JdbcTemplate(dataSource);
 		this.dataSource		= dataSource;
 	}
 	
@@ -26,32 +33,24 @@ public class UserDao {
 	}
 	
 	public void add(final User user) throws SQLException, ClassNotFoundException {
-		this.jdbcContext.workWithStatementStrategy(
-			new StatementStrategy() {
-			
-				public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-						
-					PreparedStatement	ps	= c.prepareStatement(
-							"insert into users(id, name, password) values(?,?,?)");
-					
-					ps.setString(1, user.getId());
-					ps.setString(2, user.getName());
-					ps.setString(3, user.getPassword());
-					
-					return ps;
-				}
-			}
-		);
+		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
+				user.getId(), user.getName(), user.getPassword());
 	}
 	
 	public void deleteAll() throws SQLException {
 		
-		this.jdbcContext.executeSql("delete from users");
+		this.jdbcTemplate.update(
+			new PreparedStatementCreator() {
+				
+				public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+					return con.prepareStatement("delete from users");
+				}
+			}
+		);
 	}	
 	
 	public User get(String id) throws SQLException, ClassNotFoundException {
 		
-//		Connection			c		= connectionMaker.makeConnection();
 		Connection			c	= dataSource.getConnection();
 		
 		PreparedStatement	ps		= c.prepareStatement(
@@ -86,40 +85,19 @@ public class UserDao {
 	
 	
 	public int getCount() throws SQLException {
-		Connection			c	= null;
-		PreparedStatement	ps	= null;
-		ResultSet			rs	= null;
-		
-		try {
-			c	= dataSource.getConnection();
-			ps	= c.prepareStatement("select count(*) from users");
-			rs	= ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-				}
+		return this.jdbcTemplate.query(new PreparedStatementCreator() {
+			
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				return con.prepareStatement("select count(*) from users");
 			}
+		}, new ResultSetExtractor<Integer>() {
 
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
+			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+				rs.next();
+				return rs.getInt(1);
 			}
 			
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}		
-		}
+		});
 	}
 
 }
