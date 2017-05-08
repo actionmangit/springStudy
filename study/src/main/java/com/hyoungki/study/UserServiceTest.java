efec6cc363ab69ed2c8abd5d10b8b5295e218abd
kuhelhel <kuhelhel@vc.fncp.or.kr>
2017-05-08 18:16:32 +0900

    1. 테스트 로직 수정

--- study/src/main/java/com/hyoungki/study/UserServiceTest.java
+++ study/src/main/java/com/hyoungki/study/UserServiceTest.java
@@ -68,11 +68,21 @@ public class UserServiceTest {
 		
 		userService.upgradeLevels();
 		
-		checkLevel(users.get(0), Level.BASIC);
-		checkLevel(users.get(1), Level.SILVER);
-		checkLevel(users.get(2), Level.SILVER);
-		checkLevel(users.get(3), Level.GOLD);
-		checkLevel(users.get(4), Level.GOLD);
+		checkLevelUpgraded(users.get(0), false);
+		checkLevelUpgraded(users.get(1), true);
+		checkLevelUpgraded(users.get(2), false);
+		checkLevelUpgraded(users.get(3), true);
+		checkLevelUpgraded(users.get(4), false);
+	}
+	
+	private void checkLevelUpgraded(User user, boolean upgraded) {
+		User	userUpdate	= userDao.get(user.getId());
+		if (upgraded) {
+			assertThat(userUpdate.getLevel(), is(user.getLevel().nextLevel()));
+		}
+		else {
+			assertThat(userUpdate.getLevel(), is(user.getLevel()));
+		}
 	}
 	
 	private void checkLevel(User user, Level expectedLevel) {
